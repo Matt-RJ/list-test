8045a6def17adc51f475cf92ff666b5077f6db99
Matt-RJ <mntsrj@gmail.com>
2019-02-25 15:24:18 +0000

    Updated ListTest class, removed (empty) List class

--- ListTesting/test/application/ListTest.java
+++ ListTesting/test/application/ListTest.java
@@ -1,6 +1,7 @@
 package application;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
@@ -17,11 +18,13 @@ public class ListTest {
 	
 	@Before
 	public void setup() {
-		testArrayList = new ArrayList<>(Arrays.asList("Element 1", "Element 2", "Element 3,",
+		testArrayList = new ArrayList<>(Arrays.asList("Element 1", "Element 2", "Element 3",
 				"Element 4", "Element 5", "Element 6"));
-		testLinkedList = new LinkedList<>(Arrays.asList("Element 1", "Element 2", "Element 3,",
+		testLinkedList = new LinkedList<>(Arrays.asList("Element 1", "Element 2", "Element 3",
 				"Element 4", "Element 5", "Element 6"));
 	}
+	
+	// Add version 1 
 	
 	@Test
 	public void addVersion1AddsToEndOfArrayList() {
@@ -36,6 +39,8 @@ public class ListTest {
 		assertEquals(newElement, testLinkedList.get(6));
 	}
 	
+	// Add version 2
+	
 	@Test
 	public void addVersion2AddsToStartOfArrayList() {
 		String newElement = "Added Element";
@@ -48,7 +53,6 @@ public class ListTest {
 		testArrayList.add(4, newElement);
 		assertEquals(newElement, testArrayList.get(4));
 	}
-	
 	@Test
 	public void addVersion2AddsToStartOfLinkedList() {
 		String newElement = "Added Element";
@@ -62,22 +66,176 @@ public class ListTest {
 		assertEquals(newElement, testLinkedList.get(4));
 	}
 	
-	@Test
-	public void setReplacesElementInArrayList() {
-		
-		// TODO: Fix this method
-		String newElement = "Replacement Element";
-		testArrayList.set(0, newElement); 
-		String[] firstTwoElements = {"Replacement Element", "Element 2"};
-		String[] firstTwoArrayListElements = {testArrayList.get(0), testArrayList.get(1)};
-		
-		boolean elementsAreTheSame = firstTwoElements.equals(firstTwoArrayListElements);
-		
-		assertTrue(elementsAreTheSame);
-	}
+	// Set
 	
 	@Test
-	public void setReplacesElementInLinkedList() {
+	public void setReplacesElementInArrayListAtStart() {
+		String newElement = "Replacement Element";
+		testArrayList.set(0, newElement); 
+
+		boolean elementsAtIndex0AreTheSame = newElement.equals(testArrayList.get(0));
+		boolean elementsAtIndex1AreTheSame = "Element 2".equals(testArrayList.get(1));
 		
+		assertTrue(elementsAtIndex0AreTheSame && elementsAtIndex1AreTheSame);
 	}
+	@Test
+	public void setReplacesElementInArrayListAtEnd() {
+		String newElement = "Replacement Element";
+		testArrayList.set(5, newElement);
+		
+		boolean elementsAtIndex5AreTheSame = newElement.equals(testArrayList.get(5));
+		boolean elementsAtIndex4AreTheSame = "Element 5".equals(testArrayList.get(4));
+		
+		assertTrue(elementsAtIndex5AreTheSame && elementsAtIndex4AreTheSame);
+	}
+	@Test
+	public void setReplacesElementInLinkedListAtStart() {
+		String newElement = "Replacement Element";
+		testLinkedList.set(0, newElement); 
+
+		boolean elementsAtIndex0AreTheSame = newElement.equals(testLinkedList.get(0));
+		boolean elementsAtIndex1AreTheSame = "Element 2".equals(testLinkedList.get(1));
+		
+		assertTrue(elementsAtIndex0AreTheSame && elementsAtIndex1AreTheSame);
+	}
+	@Test
+	public void setReplacesElementInLinkedListAtEnd() {
+		String newElement = "Replacement Element";
+		testLinkedList.set(5, newElement);
+		
+		boolean elementsAtIndex5AreTheSame = newElement.equals(testLinkedList.get(5));
+		boolean elementsAtIndex4AreTheSame = "Element 5".equals(testLinkedList.get(4));
+		
+		assertTrue(elementsAtIndex5AreTheSame && elementsAtIndex4AreTheSame);
+	}
+	
+	// Get
+	
+	@Test
+	public void getReturnsFirstElementInArrayList() {
+		assertTrue(testArrayList.get(0).equals("Element 1"));
+	}
+	@Test
+	public void getReturnsMiddleElementInArrayList() {
+		assertTrue(testArrayList.get(3).equals("Element 4"));
+	}
+	@Test
+	public void getReturnsLastElementInArrayList() {
+		assertTrue(testArrayList.get(5).equals("Element 6"));
+	}
+	@Test
+	public void getReturnsFirstElementInLinkedList() {
+		assertTrue(testLinkedList.get(0).equals("Element 1"));
+	}
+	@Test
+	public void getReturnsMiddleElementInLinkedList() {
+		assertTrue(testLinkedList.get(3).equals("Element 4"));
+	}
+	@Test
+	public void getReturnsLastElementInLinkedList() {
+		assertTrue(testLinkedList.get(5).equals("Element 6"));
+	}
+	
+	// IndexOf
+	
+	@Test
+	public void indexOfReturnsFirstIndexInArrayList() {
+		assertEquals(0,testArrayList.indexOf("Element 1"));
+	}
+	@Test
+	public void indexOfReturnsMiddleIndexInArrayList() {
+		assertEquals(3,testArrayList.indexOf("Element 4"));
+	}
+	@Test
+	public void indexOfReturnsLastIndexInArrayList() {
+		assertEquals(5,testArrayList.indexOf("Element 6"));
+	}
+	@Test
+	public void indexOfReturnsFirstIndexInLinkedList() {
+		assertEquals(0,testLinkedList.indexOf("Element 1"));
+	}
+	@Test
+	public void indexOfReturnsMiddleIndexInLinkedList() {
+		assertEquals(3,testLinkedList.indexOf("Element 4"));
+	}
+	@Test
+	public void indexOfReturnsLastIndexInLinkedList() {
+		assertEquals(5,testLinkedList.indexOf("Element 6"));
+
+	}
+	
+	// Remove Version 1
+	
+	@Test
+	public void removeVersion1RemovesFirstElementInArrayList() {
+		testArrayList.remove(0);
+		assertFalse(testArrayList.contains("Element 1"));
+	}
+	@Test
+	public void removeVersion1RemovesMiddleElementInArrayList() {
+		testArrayList.remove(3);
+		assertFalse(testArrayList.contains("Element 4"));
+	}
+	@Test
+	public void removeVersion1RemovesLastElementInArrayList() {
+		testArrayList.remove(5);
+		assertFalse(testArrayList.contains("Element 6"));
+	}
+	@Test
+	public void removeVersion1RemovesFirstElementInLinkedList() {
+		testLinkedList.remove(0);
+		assertFalse(testLinkedList.contains("Element 1"));
+	}
+	@Test
+	public void removeVersion1RemovesMiddleElementInLinkedList() {
+		testLinkedList.remove(3);
+		assertFalse(testLinkedList.contains("Element 4"));
+	}
+	@Test
+	public void removeVersion1RemovesLastElementInLinkedList() {
+		testLinkedList.remove(5);
+		assertFalse(testLinkedList.contains("Element 6"));
+	}
+	
+	// Remove Version 2
+	
+	@Test
+	public void removeVersion2RemovesFirstElementInArrayList() {
+		testArrayList.remove("Element 1");
+		assertFalse(testArrayList.contains("Element 1"));
+	}
+	@Test
+	public void removeVersion2RemovesMiddleElementInArrayList() {
+		testArrayList.remove("Element 3");
+		assertFalse(testArrayList.contains("Element 3"));
+	}
+	@Test
+	public void removeVersion2RemovesLastElementInArrayList() {
+		testArrayList.remove("Element 6");
+		assertFalse(testArrayList.contains("Element 6"));
+	}
+	@Test
+	public void removeVersion2RemovesFirstElementInLinkedList() {
+		testLinkedList.remove("Element 1");
+		assertFalse(testLinkedList.contains("Element 1"));
+	}
+	@Test
+	public void removeVersion2RemovesMiddleElementInLinkedList() {
+		testLinkedList.remove("Element 3");
+		assertFalse(testLinkedList.contains("Element 3"));
+	}
+	@Test
+	public void removeVersion2RemovesLastElementInLinkedList() {
+		testLinkedList.remove("Element 6");
+		assertFalse(testLinkedList.contains("Element 6"));
+	}
+	
+	// Contains
+	
+	@Test
+	public void containsFindsElementInArrayList() {
+		// TODO
+	}
+	
 }
+
